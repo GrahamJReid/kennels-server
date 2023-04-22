a6ebca0f3d16e71cb2153def77492ed7539e9a08
GrahamJReid <grahamreidtechnology@gmail.com>
2023-04-22 09:28:49 -0500

    Delete Locations Functional

--- views/__init__.py
+++ views/__init__.py
@@ -4,6 +4,7 @@ from .animal_requests import create_animal
 from .animal_requests import delete_animal
 from .location_requests import get_all_locations
 from .location_requests import create_location
+from .location_requests import delete_location
 from .location_requests import get_single_location
 from .employee_requests import get_all_employees
 from .employee_requests import get_single_employee
