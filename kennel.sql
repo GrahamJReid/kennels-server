25da9b05866b09a7751dbf062a29e5e0feddd643
GrahamJReid <grahamreidtechnology@gmail.com>
2023-04-24 13:16:54 -0500

    customers updated with sql table

--- kennel.sql
+++ kennel.sql
@@ -4,6 +4,14 @@ CREATE TABLE `Location` (
 	`address`	TEXT NOT NULL
 );
 
+CREATE TABLE `Customer` (
+    `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `name`    TEXT NOT NULL,
+    `address`    TEXT NOT NULL,
+    `email`    TEXT NOT NULL,
+    `password`    TEXT NOT NULL
+);
+
 CREATE TABLE `Animal` (
 	`id`  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	`name`  TEXT NOT NULL,
@@ -32,6 +40,11 @@ INSERT INTO `Employee` VALUES (null, "Kristen Norris", "100 Main St", 1);
 INSERT INTO `Employee` VALUES (null, "Meg Ducharme", "404 Unknown Ct", 2);
 INSERT INTO `Employee` VALUES (null, "Hannah Hall", "204 Empty Ave", 1);
 INSERT INTO `Employee` VALUES (null, "Leah Hoefling", "200 Success Way", 2);
+
+INSERT INTO `Customer` VALUES (null, "Mo Silvera", "201 Created St", "mo@silvera.com", "password");
+INSERT INTO `Customer` VALUES (null, "Bryan Nilsen", "500 Internal Error Blvd", "bryan@nilsen.com", "password");
+INSERT INTO `Customer` VALUES (null, "Jenna Solis", "301 Redirect Ave", "jenna@solis.com", "password");
+INSERT INTO `Customer` VALUES (null, "Emily Lemmon", "454 Mulberry Way", "emily@lemmon.com", "password");
 
 INSERT INTO `Animal` VALUES (null, "Snickers", "Recreation", "Dalmation", 4, 1);
 INSERT INTO `Animal` VALUES (null, "Jax", "Treatment", "Beagle", 1, 1);
