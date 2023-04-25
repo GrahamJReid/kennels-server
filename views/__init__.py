ce08f1f5bb0908bc11c67ea8194670ee28448137
GrahamJReid <grahamreidtechnology@gmail.com>
2023-04-24 20:50:48 -0500

    can search customers by Email

--- views/__init__.py
+++ views/__init__.py
@@ -19,3 +19,4 @@ from .customer_requests import get_single_customer
 from .customer_requests import create_customer
 from .customer_requests import delete_customer
 from .customer_requests import update_customer
+from .customer_requests import get_customer_by_email
