cac034a247a48319db7946d2d2880239c0749eea
GrahamJReid <grahamreidtechnology@gmail.com>
2023-04-24 19:27:57 -0500

    able to get all locations

--- views/location_requests.py
+++ views/location_requests.py
@@ -1,3 +1,7 @@
+import sqlite3
+import json
+from models import Location
+
 LOCATIONS = [
     {
         "id": 1,
@@ -11,7 +15,42 @@ LOCATIONS = [
     }
 ]
 def get_all_locations():
-    return LOCATIONS
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address
+           
+        FROM location a
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        locations = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            location = Location(row['id'], row['name'], row['address'],
+                           )
+
+            locations.append(location.__dict__) # see the notes below for an explanation on this line of code.
+
+    return locations
   # Function with a single parameter
 def get_single_location(id):
     # Variable to hold the found location, if it exists
